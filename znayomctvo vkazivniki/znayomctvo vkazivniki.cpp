153f733728b08949a57b3a02d83ea6eb586d807c
lolololololol931 <kirill2008tuy@gmail.com>
2025-01-08 13:46:12 +0200

    n1 done

--- znayomctvo vkazivniki/znayomctvo vkazivniki.cpp
+++ znayomctvo vkazivniki/znayomctvo vkazivniki.cpp
@@ -6,5 +6,17 @@ int main()
 {
     system("chcp 1251>null");
     //n1
+    /*const int size = 5;
+    int ar1[size] = { 1,2,3,4,5 };
+    int insertar[size];
+    for (int i = 0; i < size; i++) {
+        *(insertar + i) = *(ar1 + i);
+    }
+
+    cout << "coppied: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << insertar[i] << " ";
+    }*/
+
 }
 
